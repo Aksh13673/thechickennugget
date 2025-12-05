a5f723db30073f64f12c5c5dee1a2f37fd6be8b9
Aksheita Dholakia <aksheitadholakia@gmail.com>
2025-12-05 16:55:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+<script>
+    // restart animation on reload (optional)
+    const man = document.getElementById("man");
+    man.style.animation = "none";
+    man.offsetHeight; 
+    man.style.animation = "run 4s linear forwards";
+</script>
